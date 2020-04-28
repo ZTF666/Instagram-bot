a665ec8f4f585cf82659b0948b3002e73ad39938
ZTF <ZTF666@protonmail.ch>
2020-04-28 07:15:16 +0000

    :robot:

--- downloadImages.py
+++ downloadImages.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
-from credentials import login,pwd,profile
+from credentials import profile
 import urllib.request
 
 
